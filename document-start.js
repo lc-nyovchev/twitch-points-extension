90e4098bdaa41117c4105eb5adc30533bf07f6e1
nyo <1153009+lc-nyovchev@users.noreply.github.com>
2023-09-21 19:47:24 +0200

    Added prettier and prepared script namespaces

--- document-start.js
+++
@@ -1,7 +0,0 @@
-const injectScript = () => {
-    const script = document.createElement('script')
-    script.src = chrome.runtime.getURL('twitch-points-extension.js')
-    ;(document.head || document.documentElement).appendChild(script)
-}
-
-injectScript()
