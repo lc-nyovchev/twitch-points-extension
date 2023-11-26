bc96706d27792a1759484da88fa4b5db17dad379
nyo <1153009+lc-nyovchev@users.noreply.github.com>
2023-11-26 15:32:29 +0100

    Added clear points button

--- src/popup.js
+++ src/popup.js
@@ -2,26 +2,39 @@ class TwitchInterfaceUpdater {
     constructor(refreshInterval = 5000) {
         this.refreshInterval = refreshInterval
     }
+
     get pointsValuesSelector() {
         return '.points-values'
     }
+
     async getPoints() {
         const pointsRaw = await chrome.storage.sync.get()
         return Object.entries(pointsRaw).map(([channelName, score]) => {
-            return { channelName, score }
+            return {channelName, score}
         })
     }
+
     async updateInterface() {
         const points = await this.getPoints()
         const pointsValuesDiv = document.querySelector(this.pointsValuesSelector)
-        pointsValuesDiv.innerHTML = '<tr><th>Channel Name</th><th>Points</th></tr>'
+        pointsValuesDiv.innerHTML = '<tr><th>Channel Name</th><th>Points</th><th>Clear</th></tr>'
         for (const point of points) {
             pointsValuesDiv.appendChild(this.createPointElement(point.channelName, point.score))
         }
     }
+
     createPointElement(channelName, score) {
         const el = document.createElement('tr')
-        el.innerHTML = `<td>${channelName}</td><td>${score}</td>`
+        el.innerHTML = `
+            <td>${channelName}</td>
+            <td>${score}</td>
+            <td><input type="button" value="x"></td>
+        `
+        const self = this
+        el.children[2].addEventListener('click', async () => {
+            await chrome.storage.sync.remove(channelName)
+            await self.updateInterface()
+        })
         return el
     }
 }
