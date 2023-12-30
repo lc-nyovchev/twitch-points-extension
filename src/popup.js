81da3071a6f2db25fd7952e14d446b0bccbb1bcd
nyo <1153009+lc-nyovchev@users.noreply.github.com>
2023-12-30 16:26:20 +0100

    Initial van implementation

--- src/popup.js
+++ src/popup.js
@@ -1,41 +1,17 @@
-const {tr, td, th, input} = van.tags
+const {div, tr, td, th, input} = van.tags
 
-class TwitchInterfaceUpdater {
-    constructor(refreshInterval = 5000, points = {}) {
-        this.refreshInterval = refreshInterval
-        this.points = points
+
+class InterfaceElementsBuilder {
+    COLOR_PALETTES = {
+        DARK: 'dark',
+        LIGHT: 'light'
     }
 
-    get pointsValuesSelector() {
-        return '.points-values'
+    constructor(twitchInterfaceUpdater) {
+        this.twitchInterfaceUpdater = twitchInterfaceUpdater
     }
 
-    async getPoints() {
-        const pointsRaw = await chrome.storage.sync.get()
-        return Object.entries(pointsRaw).map(([channelName, score]) => {
-            return {channelName, score}
-        })
-    }
-
-    async updateInterface() {
-        if (await this.isChanged()) {
-            const points = await this.getPoints()
-            const pointsValuesDiv = document.querySelector(this.pointsValuesSelector)
-            van.add(pointsValuesDiv, this.createHeader())
-            for (const point of points) {
-                van.add(pointsValuesDiv, this.createPointsRow(point.channelName, point.score))
-            }
-        }
-    }
-
-    async isChanged() {
-        const newPoints = await this.getPoints()
-        const changed = JSON.stringify(Object.entries(newPoints).sort()) !== JSON.stringify(Object.entries(this.points).sort())
-        if (changed) {
-            this.points = newPoints
-        }
-        return changed
-    }
+    colorPalette = van.state(this.COLOR_PALETTES.LIGHT)
 
     createHeader() {
         return tr(
@@ -56,11 +32,70 @@ class TwitchInterfaceUpdater {
                     value: 'x',
                     onclick: async () => {
                         await chrome.storage.sync.remove(channelName)
-                        await self.updateInterface()
+                        await self.twitchInterfaceUpdater.updateInterface()
                     }
                 })
             )
         )
+    }
+
+    createColorPaletteSwitcher() {
+        const self = this
+        return div({class: this.colorPalette},
+            this.colorPalette,
+            input({
+                type: 'button',
+                value: 'x',
+                onclick: () => {
+                    if (self.colorPalette.val === self.COLOR_PALETTES.LIGHT) {
+                        self.colorPalette.val = self.COLOR_PALETTES.DARK
+                    } else {
+                        self.colorPalette.val = self.COLOR_PALETTES.LIGHT
+                    }
+                }
+            })
+        )
+    }
+}
+
+class TwitchInterfaceUpdater {
+    constructor(refreshInterval = 5000, points = {}) {
+        this.refreshInterval = refreshInterval
+        this.points = points
+        this.interfaceElementsBuilder = new InterfaceElementsBuilder(this)
+    }
+
+    get pointsValuesSelector() {
+        return '.points-values'
+    }
+
+    async getPoints() {
+        const pointsRaw = await chrome.storage.sync.get()
+        return Object.entries(pointsRaw).map(([channelName, score]) => {
+            return {channelName, score}
+        })
+    }
+
+    async updateInterface() {
+        if (await this.isChanged()) {
+            const points = await this.getPoints()
+            const pointsValuesDiv = document.querySelector(this.pointsValuesSelector)
+            pointsValuesDiv.innerHTML = ''
+            van.add(pointsValuesDiv, this.interfaceElementsBuilder.createHeader())
+            for (const point of points) {
+                van.add(pointsValuesDiv, this.interfaceElementsBuilder.createPointsRow(point.channelName, point.score))
+            }
+            van.add(pointsValuesDiv, this.interfaceElementsBuilder.createColorPaletteSwitcher())
+        }
+    }
+
+    async isChanged() {
+        const newPoints = await this.getPoints()
+        const changed = JSON.stringify(Object.entries(newPoints).sort()) !== JSON.stringify(Object.entries(this.points).sort())
+        if (changed) {
+            this.points = newPoints
+        }
+        return changed
     }
 }
 
