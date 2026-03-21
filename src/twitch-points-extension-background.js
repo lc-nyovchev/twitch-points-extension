feb9d0d5cd7d857d59402ea5aa05c5a549e62aa4
nyo <1153009+lc-nyovchev@users.noreply.github.com>
2026-03-21 19:53:11 +0100

    chore(refactor): refactor to classes
    fix test
    
    Wip
    
    Some fixe

--- src/twitch-points-extension-background.js
+++ src/twitch-points-extension-background.js
@@ -1,8 +1,11 @@
-import { MESSAGE_CONSTANTS, EngineUtils } from './twitch-points-extension-utils.js'
+import { MESSAGE_CONSTANTS, EngineUtils } from './utils/twitch-points-extension-utils.js'
 
 class TwitchPointsStorage {
+    constructor(engineUtils = new EngineUtils(chrome)) {
+        this.engineUtils = engineUtils
+    }
     async getPoints(channelName) {
-        const scoreObj = await EngineUtils.storageGet()
+        const scoreObj = await this.engineUtils.storageGet()
         const score = parseInt(scoreObj[channelName], 10)
         if (score) {
             return score
@@ -11,21 +14,22 @@ class TwitchPointsStorage {
     }
     async setPoints(channelName, points) {
         console.debug(`Points for ${channelName} set to ${points}`)
-        return EngineUtils.storageSet({ [channelName]: points })
+        return this.engineUtils.storageSet({ [channelName]: points })
     }
 }
 
-const twitchPointsStorage = new TwitchPointsStorage()
-
-const onContentScriptMessage = async (message, sender) => {
-    if (sender.id === EngineUtils.runtime().id) {
-        if (message.type === MESSAGE_CONSTANTS.TWITCH_POINTS_MESSAGE) {
-            const channelName = message.channelName
-            const points = message.points
-            const existingPoints = await twitchPointsStorage.getPoints(channelName)
-            await twitchPointsStorage.setPoints(channelName, existingPoints + points)
+Promise.resolve(new EngineUtils(chrome)).then(engineUtils => {
+    const twitchPointsStorage = new TwitchPointsStorage(engineUtils)
+    const onContentScriptMessage = async (message, sender) => {
+        if (sender.id === EngineUtils.runtime().id) {
+            if (message.type === MESSAGE_CONSTANTS.TWITCH_POINTS_MESSAGE) {
+                const channelName = message.channelName
+                const points = message.points
+                const existingPoints = await twitchPointsStorage.getPoints(channelName)
+                await twitchPointsStorage.setPoints(channelName, existingPoints + points)
+            }
         }
     }
-}
+    engineUtils.runtime().onMessage.addListener(onContentScriptMessage)
+})
 
-EngineUtils.runtime().onMessage.addListener(onContentScriptMessage)
